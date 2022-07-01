3a232fe6e5710100765f415675ed84f2df69506f
Andrés Mateo Gabín <andres.mgabin@upm.es>
2022-07-01 11:13:11 +0200

    Some fixes to kMeans clustering
    * Initial centroids are given as an input
    
    * Fix bug for clusters with 0 points

--- Utilities/Clustering.f90
+++ Utilities/Clustering.f90
@@ -14,30 +14,24 @@ contains
 
 pure subroutine kMeans(k, x, xAvg, clusters, info)
     !* Arguments *!
-    integer,           intent(in)  :: k
-    real(wp),          intent(in)  :: x(:,:)
-    real(wp),          intent(out) :: xAvg(:,:)
-    integer,           intent(out) :: clusters(:)
-    integer, optional, intent(out) :: info
+    integer,           intent(in)    :: k
+    real(wp),          intent(in)    :: x(:,:)
+    real(wp),          intent(inout) :: xAvg(:,:)
+    integer,           intent(out)   :: clusters(:)
+    integer, optional, intent(out)   :: info
 
     !* Local variables *!
     integer, parameter   :: max_iters = 50
     integer, allocatable :: prevClusters(:)
     integer              :: nDims
     integer              :: nPts
-    integer              :: ind
     integer              :: i
 
 
     nPts  = size(x, dim=1)
     nDims = size(x, dim=2)
-    allocate(prevClusters(nPts))
 
     ! Initial centroids
-    do i = 1, k
-        ind = floor( 1.0_wp + real(i-1, kind=wp)/(k-1) * (nPts-1) )
-        xAvg(i,:) = x(ind,:)
-    end do
     call kMeans_compute_clusters(k, nDims, nPts, x, xAvg, clusters)
 
     ! Loop until convergence
@@ -105,6 +99,7 @@ pure subroutine kMeans_compute_centroids(k, nDims, nPts, x, xAvg, clusters)
         ptsInCluster(clusters(i)) = ptsInCluster(clusters(i)) + 1
     end do
     do i = 1, k
+        if (ptsInCluster(i) == 0) cycle
         xAvg(i,:) = xAvg(i,:) / real(ptsInCluster(i), kind=wp)
     end do
 
